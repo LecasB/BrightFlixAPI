f0b52157ca9359a45ba10653e521425f9f1cf5db
Leonardo Carvalho <leocarvalho@deloitte.pt>
2024-06-12 16:57:10 +0100

    fixed videos

--- Desktop/brightflix-backend/routes/videos.js
+++ Desktop/brightflix-backend/routes/videos.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/videos', (req,res) =>{
-    res.status(200).json({
-        sucess: true,
-        message: 'This route will display all videos'
-    })
-})
-
 const { getVideos } = require("../controllers/videosControl")
 
 router.route('/videos').get(getVideos)
