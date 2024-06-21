00158114cdfc4078930f79da00198d5afa35cfe6
Leonardo Carvalho <33039074+LecasB@users.noreply.github.com>
2024-06-21 15:03:02 +0100

    Delete Desktop/brightflix-backend directory

--- Desktop/brightflix-backend/routes/videos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getVideos } = require("../controllers/videosControl")
-
-router.route('/videos').get(getVideos)
-
-module.exports = router;
