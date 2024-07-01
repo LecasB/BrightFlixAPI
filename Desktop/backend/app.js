6d36d9452b0be52c292ba07c50bf17068d552139
Leonardo Carvalho <cleonardo191@gmail.com>
2024-07-01 18:02:33 +0100

    aaaaaa

--- Desktop/backend/app.js
+++ Desktop/backend/app.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
 ];
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
