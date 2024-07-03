84a2f2ee99b579b4b61a03cf22d2029a9e0fd33b
Leonardo Carvalho <cleonardo191@gmail.com>
2024-07-03 11:02:56 +0100

    aaa

--- Desktop/backend/models/series.js
+++ Desktop/backend/models/series.js
@@ -6,7 +6,7 @@ const formatDate = () => {
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
   const year = date.getFullYear();
-  return ${day}-${month}-${year};
+  return `${day}-${month}-${year}`;
 };
 
 const serieSchema = new mongoose.Schema(
